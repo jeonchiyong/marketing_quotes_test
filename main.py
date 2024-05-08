91f60fa8f0015a752a56c3c01a811faf6de9840f
jeonchiyong <59275146+jeonchiyong@users.noreply.github.com>
2024-05-08 18:48:19 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,95 @@
 # 참고 블로그 url
 # - streamlit 배포 : https://m.blog.naver.com/weisoon/223196954589
 # - langchain : https://teddylee777.github.io/langchain/langchain-tutorial-03/ , https://rfriend.tistory.com/839
+# - embedding, RAG : https://wikidocs.net/231600 ,https://wikidocs.net/233817, https://teddylee777.github.io/langchain/rag-tutorial/#%EB%8B%A8%EA%B3%84--retriever-%EC%83%9D%EC%84%B1
+
 
 # python ver : 3.11.2 64bit
 # vsc에서 local환경에 app 실행하려면 커맨드창에 {streamlit run main.py} 입력
 
 # 환경변수(github에 올릴때는 주석 처리한다.)
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# 모든 경고 메시지를 무시하도록 설정
+import warnings
+warnings.filterwarnings('ignore')
 
 # streamlit
 import streamlit as st
 
-# langchain message
+# langchain
+from langchain import hub
+from langchain_openai import OpenAIEmbeddings
+
+# document loaders
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_community.document_loaders import TextLoader
+
+# text split
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# embedding
+from langchain_community.embeddings import HuggingFaceEmbeddings, HuggingFaceBgeEmbeddings # 무료 Open Source 기반 임베딩
+
+
+# vectorstores
+from langchain_community.vectorstores import Chroma, FAISS
+from langchain_community.vectorstores.utils import DistanceStrategy
+
+# outputparser
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain_core.runnables import RunnablePassthrough
+
+
+# langchain message, Template
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.prompts import ChatPromptTemplate
 
 # ChatOpenAI
 from langchain.chat_models import ChatOpenAI
+
+# RAG 생성
+# 1. txt 로드
+loader = TextLoader("240503_KB캐피탈 상품정보.txt", encoding = 'utf-8')
+docs = loader.load()
+print(f"문서의 수: {len(docs)}")
+
+# 2. split documents
+recursive_text_splitter = RecursiveCharacterTextSplitter(
+    # 작은 청크 크기를 설정합니다.
+    chunk_size=400,
+    chunk_overlap=100,
+    length_function=len,
+    is_separator_regex=False,
+)
+split_docs = recursive_text_splitter.split_documents(docs)
+print(f"나누어진 문서의 수: {len(split_docs)}")
+print('\nsplit docs 살펴보기')
+for i in range(len(split_docs)) :
+    print(split_docs[i],'\n')
+
+
+# 3. 임베딩 객체 생성
+embeddings_model = HuggingFaceEmbeddings(
+    model_name='jhgan/ko-sbert-nli', # 사전학습 임베딩 모델
+    model_kwargs={'device':'cpu'},
+    encode_kwargs={'normalize_embeddings':True}, # 임베딩 정규화하여, 모든 벡터가 같은 범위의 값을 갖도록 함
+)
+
+# 4. 벡터스토어 생성
+vectorstore = FAISS.from_documents(
+    documents=split_docs, embedding=embeddings_model,
+    distance_strategy = DistanceStrategy.COSINE # 코사인유사도로 측정
+)
+
+# 5. Retriever 생성
+retriever = vectorstore.as_retriever(search_kwargs={'k':1}) # 단일 문서 검색
+
 
 st.image('kbc_logo.png')
 
@@ -29,7 +102,8 @@ st.text('\n')
 
 
 opt_prod = st.radio(
-    r'''$\textsf{\large ✅ STEP1. 문구를 생성하려는 대출 상품을 선택해주세요}$''',['신용대출','크로스셀', '자동차 담보 대출', '중고차 대출', '신차 대출'], horizontal=True)
+    # r'''$\textsf{\large ✅ STEP1. 문구를 생성하려는 대출 상품을 선택해주세요}$''',['신용대출','크로스셀', '자동차 담보 대출', '중고차 대출', '신차 대출'], horizontal=True)
+    r'''$\textsf{\large ✅ STEP1. 문구를 생성하려는 대출 상품을 선택해주세요}$''',['내일로 신용대출','내집으로 신용대출', '자동차 담보 대출 내차로', '소액론 신용대출', '플러스론', '탑업론'], horizontal=True)
 st.text('\n')
 
 opt_gender = st.radio(
@@ -62,9 +136,14 @@ st.text('\n')
 # llm temperature 설정 기능
 st.write(r'''$\textsf{\normalsize ✅ STEP7. 생성형 AI의 자유도를 설정해주세요}$''')
 opt_temp = st.slider(r'''$\textsf{\small\textbf {※ 자유도가 1에 가까울수록 생성형 AI가 다양한 문구를 생성합니다.}}$''', 0.0, 1.0, 0.5, step = 0.1, format="%.1f")
-print(opt_temp)
+print('선택된 temperature : ',opt_temp)
 st.text('\n')
 
+
+# Retriever 검색 결과
+query = f"""{opt_prod}에 대해 알려줘"""
+docs = retriever.get_relevant_documents(query) # 검색결과 저장
+# print(docs[0].page_content)
 
 # LLM 모델 생성
 chat_model = ChatOpenAI(model = 'gpt-3.5-turbo-0125', temperature = opt_temp)
@@ -72,13 +151,15 @@ chat_model = ChatOpenAI(model = 'gpt-3.5-turbo-0125', temperature = opt_temp)
 # gpt prompt
 # 한글
 # gpt_role_k = f"""
+# system_role
 # - 당신은 금융 마케팅 분야에 대해 풍부한 경험과 전문지식을 보유하고 있는 전문가이며, 대한민국의 금융회사인 KB캐피탈의 금융 상품 판매 활성화를 위해 마케팅 문자메시지 작성해야합니다.
-# - 마케팅 하려는 KB캐피탈 금융 상품은 '{opt_prod}'입니다.
+# - 마케팅 하려는 KB캐피탈 금융 상품은 '{opt_prod}'이며, 상품에 대한 정보는 다음 내용을 참고해주세요 : {docs}
 # - 마케팅 타겟의 성별은 '{opt_gender}', 연령대는 '{opt_age}', 직업은 '{opt_job}'입니다.
 # - 또한 마케팅 문구 생성 시 추가로 고려해야 할 사항은 '{opt_etc}'입니다.
 # - 다음 문자메시지 예시를 참고해서 작성해주세요. '대출이동서비스로 신용대출 갈아타면 금리는 낮아지고 한도는 높아질 수 있어요.', '첫 달 이자 최대 50만원의 기회! 마이너스 통장은 3만원을 드려요!'
 
-# - 다양한 어휘와 자연스러운 표현을 사용하여, 마케팅 타겟의 특성을 고려한 5개의 마케팅 문구를 작성해주십시오
+# human_role
+# - 다양한 어휘와 자연스러운 표현을 사용하고, 마케팅 타겟의 특성과 상품 정보를 고려하여 5개의 마케팅 문구를 작성해주십시오
 # - 각 문구는 50자 이내로 작성해주시고, 5개의 문구를 구분할 수 있도록 각 문구 앞에 1~5까지의 숫자를 기재해주세요.
 # - 내용이 중복되지 않고, 반복적인 문장이 생기지 않도록 자연스러운 글을 작성해주십시오.
 # - 마케팅 문구에 'KB캐피탈'이라는 단어를 반드시 포함해주십시오.
@@ -88,7 +169,7 @@ chat_model = ChatOpenAI(model = 'gpt-3.5-turbo-0125', temperature = opt_temp)
 # system 역할 부여
 system_role = f"""
 - You are an expert with extensive experience and expertise in the field of financial marketing, and you must write marketing text messages to promote sales of financial products for KB Capital, a financial company in Korea.
-- The KB Capital financial product you wish to market is {opt_prod}.
+- The KB Capital's financial product you wish to market is '{opt_prod}', and for information on the product, please refer to the following: '{docs[0].page_content}'.
 - The marketing target's gender is '{opt_gender}', age group is '{opt_age}', and occupation is '{opt_job}'.
 """
 
@@ -102,7 +183,7 @@ if opt_style != '없음' :
 
 # human role 
 human_role = f"""
-- Using a variety of vocabulary and natural expressions, please write five marketing phrases that take into account the characteristics of your marketing target.
+- Please use a variety of vocabulary and natural expressions, and write five marketing phrases considering the characteristics of your marketing target and product information.
 - Please write each phrase within 50 characters, and write a number from 1 to 5 in front of each phrase to distinguish between the 5 phrases.
 - To distinguish between the five phrases, please write a number from 1 to 5 in front of each phrase.
 - Please answer in Korean and write naturally to avoid duplication of content and repetitive sentences.
@@ -114,11 +195,11 @@ messages = [ SystemMessage(content=system_role),
             HumanMessage(content=human_role)
 ]
 
-print(messages)
+print('prompt messages : \n', messages)
 st.text('\n')
 
 # 답변 메시지 노출
-if st.button('마케팅 문구 생성하기 📋', type= 'primary') :
+if st.button('마케팅 문구 생성하기 📋', type= 'primary') : #type 옵션으로 버튼 색상 빨강색으로 설정
     with st.spinner('문구 생성 중...🤖') :
         # chat_gpt 답변 생성
         completion = chat_model.invoke(messages)
@@ -127,7 +208,10 @@ if st.button('마케팅 문구 생성하기 📋', type= 'primary') :
         # gpt가 답변한 내용을 변수에 저장
         assistant_message = completion.content
         print(assistant_message)
-        st.success('''ChatGPT가 생성한 문구입니다. 다른 문구를 보고 싶으시면,
-                   \n '마케팅 문구 생성하기'버튼을 다시 한번 눌러주세요!''')
+        st.success(f'''ChatGPT가 생성한 문구입니다. 다른 문구를 보고 싶으시면,
+                   \n '마케팅 문구 생성하기'버튼을 다시 한번 눌러주세요!
+                   \n \n \n P.S. 참고로 다음 상품 정보를 참고하여 문구를 작성했어요😁
+                   \n  {docs[0].page_content}
+                   ''')
         st.success(assistant_message)
 
